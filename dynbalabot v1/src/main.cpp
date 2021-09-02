76821699018e29b1dc1ae59d6d12d120ad622b9c
VitRuzicka <36342077+VitRuzicka@users.noreply.github.com>
2021-09-02 22:07:33 +0200

    Add files via upload

--- dynbalabot v1/src/main.cpp
+++ dynbalabot v1/src/main.cpp
@@ -188,10 +188,74 @@ if (PID) {
     }
 }
 */
+#include <AccelStepper.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+AccelStepper stepper1(1, 2,13);
+AccelStepper stepper2(1, 32,33); 
+
+const char* ssid = "Net";
+const char* password = "ruzicka123456789";
 
 void setup(){
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+  ArduinoOTA.setHostname("dynbalabot");
 
+
+
+    
+ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+    stepper1.setMaxSpeed(1000);
+    stepper1.setAcceleration(200);
+    stepper2.setMaxSpeed(1000);
+    stepper2.setAcceleration(200);
+    stepper1.setSpeed(500);
+    stepper2.setSpeed(500);
 }
 void loop(){
-
+ArduinoOTA.handle();
+stepper1.runSpeed();
+stepper2.runSpeed();
 }
