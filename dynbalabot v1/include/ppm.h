b62e5de8f8fe9d7a04fa063edc6e97eb6315c2d4
VitRuzicka <36342077+VitRuzicka@users.noreply.github.com>
2021-11-02 19:23:48 +0100

    Add files via upload

---
+++ dynbalabot v1/include/ppm.h
@@ -0,0 +1,8 @@
+#pragma once
+
+extern int ch[7];
+
+
+uint clip(int, int,int);
+void cteni_signalu();
+void zpracovani_signalu();
--- dynbalabot v1/src/funkcniPPM.cpp
+++ dynbalabot v1/src/funkcniPPM.cpp
@@ -1,7 +1,10 @@
-/*
+
 #include<Arduino.h>
+#include"ppm.h"
+
 unsigned long int soucasnaProdleva,predchoziProdleva,prodleva;
-int x[15],ch1[15],ch[7],ind;
+int x[15],ch1[15],ind;
+int ch[7];
 
 uint clip(int vstup, int max, int min){
   if(vstup > max){
@@ -38,22 +41,6 @@ int i,j,k=0;
     ch[i]=(ch1[i+j]-1000);}   //dosad sest hodnot bez separacni pauzy
   }
 
-void setup() {
-Serial.begin(115200);
-  pinMode(2, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(18), cteni_signalu, FALLING); //ve skutecnosti se jedna o vzestupnou hranu protoze pin je v PULLUP rezimu
-}
 
-void loop() {
-zpracovani_signalu(); //nutno zavolat pred pouzitim signalu z promenne pro "update udaju"
 
-Serial.print(clip(ch[1], 1500, 0));Serial.print("\t");
-Serial.print(ch[2]);Serial.print("\t");
-Serial.print(ch[3]);Serial.print("\t");
-Serial.print(ch[4]);Serial.print("\t");
-Serial.print(ch[5]);Serial.print("\t");
-Serial.print(ch[6]);Serial.print("\n");
 
-delay(100);
-}
-*/
