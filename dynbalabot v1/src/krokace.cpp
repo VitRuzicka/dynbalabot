14293bda85669ad782ee0731b2ce6ac09ee6ee38
VitRuzicka <36342077+VitRuzicka@users.noreply.github.com>
2021-12-01 23:10:46 +0100

    Add files via upload

--- dynbalabot v1/src/krokace.cpp
+++ dynbalabot v1/src/krokace.cpp
@@ -1,8 +1,10 @@
 #include "prevodovka.h"
 #include <AccelStepper.h>
+#define ENDPIN1 34
+#define ENDPIN2 35 
 
-AccelStepper stepper1(1, 2,13);
-AccelStepper stepper2(1, 32,33); 
+AccelStepper stepper1(1, 32,33);
+AccelStepper stepper2(1, 25,25); 
 
 void nastav_krokace(){
     stepper1.setMaxSpeed(MAX_SPEED);
