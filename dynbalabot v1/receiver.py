4bc365e1e4052acb076d4046c9e6e9238f6977b3
VitRuzicka <36342077+VitRuzicka@users.noreply.github.com>
2021-12-02 11:17:10 +0100

    prijimac dat pro python
    -otesetovat filtrovani v html
    -vytvorit filtrovani dat v pythonu
    -pridat vice diag dat do esp kodu
    -graficke prostredi pro python

---
+++ dynbalabot v1/receiver.py
@@ -0,0 +1,36 @@
+from ws4py.client.threadedclient import WebSocketClient
+import time, requests, json, os
+#vytvořit popup menu se jmény hostů a jejich adresami ->  výběr hosta, připojení k jeho adrese
+host = "dynbalabot.local" #example
+websocketHost = "ws://" + host + "/ws"
+#host = "ws://192.168.0.160/ws"
+
+class Klient(WebSocketClient):
+    def opened(self):
+        print("Spojeni navazano")
+    def closed(self, code, reason=None):
+        print("Spojeni ukonceno ", code, reason)
+    def received_message(self, obsah):
+        print(obsah)
+        zprava = json.loads(obsah)
+        try:
+            print(zprava['akce'])
+        except IndexError: #upravit
+            print("hodnota nenalezena v poli")
+            
+            
+if __name__ == '__main__':
+    try:
+        response = os.system("ping -c 1 -w 150 " + host) #pokud se lze pingnout na zarizeni pokraacuj dal 
+        if response == 0:
+            #lze se pripojit
+            ws = Klient(websocketHost) #pripoj se pres websockety
+            ws.connect()
+            print("Ready !")
+        else:
+            print("nelze se pripojit")
+            exit()
+       
+    except KeyboardInterrupt:
+        ws.close()
+        exit()
